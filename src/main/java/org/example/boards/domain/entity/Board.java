f4bddbf7af21e7ca1fd402ffefaa32c7c55cd838
hunko <rhwlgns4386@naver.com>
2025-03-18 12:53:08 +0900

    refactor : 업데이트 날짜 테스트편의성을 위해 title객체로 업데이트 변경

--- src/main/java/org/example/boards/domain/entity/Board.java
+++ src/main/java/org/example/boards/domain/entity/Board.java
@@ -1,18 +1,35 @@
 package org.example.boards.domain.entity;
 
+import java.time.LocalDateTime;
+
 import static org.example.article.domain.entity.StringValidator.validate;
 
 public class Board {
 
     private Long id;
     private String title;
+    private LocalDateTime updateAt;
 
-    public Board(String title) {
-        validate(title);
-        this.title = title;
+    public Board(Title title) {
+        setTitle(title);
+    }
+
+
+    private void setTitle(Title title) {
+        validate(title.toStringValue());
+        this.title = title.toStringValue();
+        this.updateAt = title.createAt();
     }
 
     public String title() {
         return title;
+    }
+
+    public void update(Title title) {
+        setTitle(title);
+    }
+
+    public LocalDateTime updateAt() {
+        return updateAt;
     }
 }
--- src/test/java/org/example/boards/domain/entity/BoardTest.java
+++ src/test/java/org/example/boards/domain/entity/BoardTest.java
@@ -5,19 +5,17 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EmptySource;
 import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.junit.jupiter.params.shadow.com.univocity.parsers.annotations.NullString;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
-import static org.junit.jupiter.api.Assertions.*;
 
 public class BoardTest {
 
     @Test
-    void 보드생성(){
+    void 보드생성() {
         String title = "제목";
 
-        Board board = new Board(title);
+        Board board = new Board(new Title(title));
 
         assertThat(board.title()).isEqualTo(title);
     }
@@ -25,8 +23,28 @@ public class BoardTest {
     @ParameterizedTest
     @NullSource
     @EmptySource
-    @ValueSource(strings = {" ","   "})
-    void 제목없이_제목_생성시_예외(String title){
-        assertThatIllegalArgumentException().isThrownBy(() -> new Board(title));
+    @ValueSource(strings = {" ", "   "})
+    void 제목없이_제목_생성시_예외(String title) {
+        assertThatIllegalArgumentException().isThrownBy(() -> new Board(new Title(title)));
+    }
+
+    @Test
+    void 보드수정() {
+        Board board = new Board(new Title("title"));
+        Title title = new Title("제목");
+
+        board.update(title);
+
+        assertThat(board.title()).isEqualTo(title.toStringValue());
+        assertThat(board.updateAt()).isEqualTo(title.createAt());
+    }
+
+    @ParameterizedTest
+    @NullSource
+    @EmptySource
+    @ValueSource(strings = {" ", "   "})
+    void 제목없이_제목으로_수정시_예외(String title) {
+        Board board = new Board(new Title("title"));
+        assertThatIllegalArgumentException().isThrownBy(() -> board.update(new Title(title)));
     }
 }
