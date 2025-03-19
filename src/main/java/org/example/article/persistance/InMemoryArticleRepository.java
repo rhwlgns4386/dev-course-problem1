bedf8061fd6c06ae385b2487c310d4cd567ebc2d
hunko <rhwlgns4386@naver.com>
2025-03-19 09:09:37 +0900

    chore : 주석 이동

--- src/main/java/org/example/article/persistance/InMemoryArticleRepository.java
+++ src/main/java/org/example/article/persistance/InMemoryArticleRepository.java
@@ -4,16 +4,6 @@ import org.example.article.domain.entity.Article;
 import org.example.article.domain.service.ArticleRepository;
 import org.example.persistance.LongKeyBaseRepository;
 
-/**
- * 이 저장소는 인메모리에 저장하며 시스템을 재부팅시 데이터가 소실됩니다.
- * 또한 멀티스레드환경에서 동시성이 고려되지 않아,
- * 데이터 불일치나 예상치 못한 동작을 할 수 있습니다.
- *
- * @author 고지훈
- * @version 1.0
- * @since 2025-03-13
- */
-
 public class InMemoryArticleRepository extends LongKeyBaseRepository<Article> implements ArticleRepository {
 
 }
--- src/main/java/org/example/persistance/LongKeyBaseRepository.java
+++ src/main/java/org/example/persistance/LongKeyBaseRepository.java
@@ -4,6 +4,15 @@ import org.example.article.persistance.exception.IdFieldNotFoundException;
 
 import java.util.*;
 
+/**
+ * 이 저장소는 인메모리에 저장하며 시스템을 재부팅시 데이터가 소실됩니다.
+ * 또한 멀티스레드환경에서 동시성이 고려되지 않아,
+ * 데이터 불일치나 예상치 못한 동작을 할 수 있습니다.
+ *
+ * @author 고지훈
+ * @version 1.0
+ * @since 2025-03-13
+ */
 public class LongKeyBaseRepository<E> implements BaseRepository<Long,E>{
     protected final Map<Long,E> store = new HashMap<>();
     private final LongIdGenerator idGenerator = new LongIdGenerator();
